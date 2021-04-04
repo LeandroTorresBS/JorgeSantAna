056a59967ad6c473b97bfad09e2ddabd801f1203
Leandro Torres Batista da Silva <leandrotorresbs@gmail.com>
2021-04-04 20:34:45 -0300

    Adding Routes

--- app2/src/app/app.module.ts
+++ app2/src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import { ROUTES } from './app.routes';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+import { OfertaComponent } from './oferta/oferta.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     HomeComponent,
-    RodapeComponent
+    RodapeComponent,
+    RestaurantesComponent,
+    DiversaoComponent,
+    OfertaComponent,
+    ComoUsarComponent,
+    OndeFicaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ app2/src/app/ofertas.service.ts
@@ -0,0 +1,69 @@
+import { Oferta } from './shared/oferta.model';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { URL_API } from './app.api';
+
+
+@Injectable()
+export class OfertasService {
+
+    //private url_api = 'http://localhost:3000/ofertas'
+
+    constructor(private http: HttpClient) {
+
+    }
+
+
+    public getOfertas(): Promise<Oferta[]> {
+        //efetura uma requisição HTTP
+        return this.http.get(`${URL_API}/ofertas?destaque=true`).toPromise()
+            .then((resposta: any) => resposta)
+        //retornar um promise Oferta[]
+    }
+
+    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`).toPromise()
+            .then((resposta: any) => resposta)
+    }
+
+    public getOfertaPorId(id: number): Promise<Oferta> {
+        return this.http.get(`${URL_API}/ofertas?id=${id}`).toPromise()
+            .then((resposta: any) => resposta[0])
+    }
+
+    public getComoUsarOfertaPorId(id: number): Promise<string> {
+        return this.http.get(`${URL_API}/como-usar?id=${id}`).toPromise()
+            .then((resposta: any) => resposta[0].descricao)
+    }
+
+    public getOndeFicaOfertaPorId(id: number): Promise<string> {
+        return this.http.get(`${URL_API}/onde-fica?id=${id}`).toPromise()
+            .then((resposta: any) => resposta[0].descricao)
+    }
+
+    // public getOfertas2(): Promise<Oferta[]> {
+    //     return new Promise((resolve, reject) => {
+    //         //algum tipo de processamento, que ao finalizar, chama a função resolve ou a função reject
+    //         let funfou = true
+
+    //         if (funfou) {
+    //             setTimeout(() => resolve(this.ofertas), 3000)
+    //         } else {
+    //             reject({ codigo_erro: 404, mensagem_erro: 'Servidor não encontrado' })
+    //         }
+    //     })
+    //         .then((ofertas: Oferta[]) => {
+    //             //fazer alguma tratativa
+    //             return ofertas
+    //         })
+    //         .then((ofertas: Oferta[]) => {
+    //             //fazer outra tratativa
+    //             return new Promise((resolve2, reject2) => {
+    //                 setTimeout(() => { resolve2(ofertas) }, 3000)
+    //             })
+    //         })
+    //         .then((ofertas: Oferta[]) => {
+    //             return ofertas
+    //         })
+    // }
+}
